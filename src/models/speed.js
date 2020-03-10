956d03007891b36e11ace492dc78628e7695ad7b
Jordie Little <dev@pkmntcgvault.com>
2020-03-10 20:55:37 +1000

    Fixed issue where speedtest timeout would mess with Stats and the Graph

--- src/models/speed.js
+++ src/models/speed.js
@@ -7,7 +7,6 @@ exports.last = function(interval, done) {
     var lines = data.toString().split("\n");
     var res = [[],[],[],[]];
     var start = 0;
-    if (lines.length > 250) start = lines.length - 251;
     for (i = start; i < lines.length; i++) {
       var line = lines[i];
       if(line && line.length > 0) {
@@ -16,9 +15,12 @@ exports.last = function(interval, done) {
           var times = items[0].toString().replace('  ', ' ').split(' ');
           var clock = times[3].split(':');
           res[0].push(times[2] + '-' + times[1] + '-' + times[4] + ' ' + (parseInt(clock[0]) + 1).toString() + ':' + clock[1] + ' [' + times[0] + ']');
-          res[1].push(items[1]);
-          res[2].push(items[2]);
-          res[3].push(items[3]);
+          //Dev note: we do what we do below to the ping because if the script
+          //times out for any reason, we will get 0 for up/download but the ping
+          //will timeout and record 1800000 seconds, which screws stats and the graph
+          res[1].push((items[1] > 2000) ? 0 : items[1]); //Ping
+          res[2].push(items[2]); //Down
+          res[3].push(items[3]); //Up
         }
       }
     }
